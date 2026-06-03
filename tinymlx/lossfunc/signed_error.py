7ec7447720981a5972fcb06e2f29849ef9ad0a07
iamprasadraju <gls.prasadraju@gmail.com>
2026-06-03 15:07:28 +0530

    UPDATE -> BACKWARD

--- tinymlx/lossfunc/signed_error.py
+++ tinymlx/lossfunc/signed_error.py
@@ -1,6 +1,8 @@
 # signed error: (x - y)
-def signed_error(true_value, predict_value):
-    """
-        Measures raw deviation; allows for cancellation.
-    """
-    return true_value - predict_value
+
+class SignedError:   
+    def __call__(self, true_value, predict_value):
+        """
+            Measures raw deviation; allows for cancellation.
+        """
+        return true_value - predict_value
