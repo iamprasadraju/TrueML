5f342edb98c5050e005ff964bee1f4ac3be62b4b
iamprasadraju <gls.prasadraju@gmail.com>
2026-06-18 11:21:58 +0530

    cleanup from previous merge

--- tinymlx/lossfunc/signed_error.py
+++
@@ -1,6 +0,0 @@
-# signed error: (x - y)
-def signed_error(true_value, predict_value):
-    """
-        Measures raw deviation; allows for cancellation.
-    """
-    return true_value - predict_value
