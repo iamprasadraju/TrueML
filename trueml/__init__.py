89d4e4d52ba66ec83107d98be89536e8dc82e139
iamprasadraju <gls.prasadraju@gmail.com>
2026-06-19 15:18:37 +0530

    renamed module name and file in visualize

--- trueml/__init__.py
+++ trueml/__init__.py
@@ -4,4 +4,5 @@ __all__ = [
     "linearmodel",
     "losses",
     "errors",
+    "visualize",
 ]
--- trueml/visualize/plot.py
+++ trueml/visualize/plot.py
@@ -2,5 +2,6 @@ import plotly.graph_objects as go
 from IPython.display import display
 
 def plot(losses, epochs):
+    pass
     
     
