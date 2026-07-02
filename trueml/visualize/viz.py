df5a43fd9baf6030fe37a4f490e25e1131b75d23
iamprasadraju <gls.prasadraju@gmail.com>
2026-07-02 09:47:36 +0530

    feat: add figsize parameter to viz2d and viz3d

--- trueml/visualize/viz.py
+++ trueml/visualize/viz.py
@@ -2,7 +2,7 @@ import numpy as np
 import plotly.graph_objects as go
 
 
-def viz2d(func, x_range=(-5, 5), resolution=50):
+def viz2d(func, x_range=(-5, 5), resolution=50, figsize=(500, 500)):
     """
     Visualize a single-variable function as a 2D line plot.
 
@@ -17,6 +17,9 @@ def viz2d(func, x_range=(-5, 5), resolution=50):
     resolution : int, default=50
         Number of points sampled along the x-axis.
 
+    figsize : tuple, default=(500, 500)
+        Figure size as (width, height) in pixels.
+
     Examples
     --------
     >>> from trueml.activations import sigmoid
@@ -28,15 +31,18 @@ def viz2d(func, x_range=(-5, 5), resolution=50):
     fig = go.Figure(data=[go.Scatter(x=x, y=y, mode="lines")])
     plot_name = func.__qualname__.split(".")[0]
     fig.update_layout(
+        template="plotly_white",
         title=plot_name,
         xaxis_title="x",
         yaxis_title="value",
+        width=figsize[0],
+        height=figsize[1],
     )
 
     fig.show()
 
 
-def viz3d(func, x_range=(-5, 5), y_range=(-5, 5), resolution=50):
+def viz3d(func, x_range=(-5, 5), y_range=(-5, 5), resolution=50, figsize=(500, 500)):
     """
     Visualize a two-variable function as a 3D surface.
 
@@ -54,6 +60,9 @@ def viz3d(func, x_range=(-5, 5), y_range=(-5, 5), resolution=50):
     resolution : int, default=50
         Number of points sampled along each axis.
 
+    figsize : tuple, default=(500, 500)
+        Figure size as (width, height) in pixels.
+
     Examples
     --------
     >>> viz3d(lambda x, y: np.abs(x - y))
@@ -64,10 +73,13 @@ def viz3d(func, x_range=(-5, 5), y_range=(-5, 5), resolution=50):
     X, Y = np.meshgrid(x, y)
     Z = func(X, Y)
 
-    fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)])
+    fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z, colorscale="viridis")])
     plot_name = func.__qualname__.split(".")[0]
     fig.update_layout(
+        template="plotly_white",
         title=plot_name,
+        width=figsize[0],
+        height=figsize[1],
         scene=dict(xaxis_title="y_true", yaxis_title="y_pred", zaxis_title="value"),
     )
 
