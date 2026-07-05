e6479a256bcebce21d851fae01fe1b3e3f3528dc
iamprasadraju <gls.prasadraju@gmail.com>
2026-07-05 22:57:21 +0530

    porting viz from plotly to matplotlib for vizual

--- trueml/__init__.py
+++ trueml/__init__.py
@@ -1,9 +1,9 @@
 from . import errors, linearmodel, losses
 
-__version__ = "0.0.1"
 __all__ = [
     "linearmodel",
     "losses",
     "errors",
-    "visualize",
+    "viz",
+    "activations",
 ]
