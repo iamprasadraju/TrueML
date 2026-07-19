aec865c4af01b885bb951afac2986950e76ae54f
iamprasadraju <gls.prasadraju@gmail.com>
2026-07-19 13:02:27 +0530

    small refactor and uv config

--- trueml/preprocessing/__init__.py
+++ trueml/preprocessing/__init__.py
@@ -1,9 +1,11 @@
 from .data_split import train_test_split
 from .encoding import LabelEncoder
 from .scaling import MinMaxScaler
+from .impute import SimpleImputer
 
 __all__ = [
     "train_test_split",
     "LabelEncoder",
     "MinMaxScaler",
+    "SimpleImputer",
 ]
