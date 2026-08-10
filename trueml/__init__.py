274dec4a4f42f81bfa73c6e79ea96a86e3063cad
iamprasadraju <gls.prasadraju@gmail.com>
2026-08-10 22:49:18 +0530

    remove encoding module, may be added later

--- trueml/__init__.py
+++ trueml/__init__.py
@@ -1,8 +1,7 @@
-from . import errors, linear_model, losses, plot, preprocessing
+from . import linear_model, losses, plot, preprocessing
 
 __all__ = [
     "activations",
-    "errors",
     "linear_model",
     "losses",
     "plot",
--- trueml/preprocessing/__init__.py
+++ trueml/preprocessing/__init__.py
@@ -1,10 +1,8 @@
 from .data_split import train_test_split
-from .encoding import LabelEncoder
 from .impute import SimpleImputer
 from .scaling import MinMaxScaler
 
 __all__ = [
-    "LabelEncoder",
     "MinMaxScaler",
     "SimpleImputer",
     "train_test_split",
